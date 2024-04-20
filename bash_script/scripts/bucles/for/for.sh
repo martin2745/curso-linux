b4df99119f4f98c666730f279df0dae0027b3a17
MARTÍN GIL BLANCO <gilblancomartin@gmail.com>
2024-04-20 12:21:16 +0200

    for con seq y número de saltos

--- bash_script/scripts/bucles/for/for.sh
+++ bash_script/scripts/bucles/for/for.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Tambien puedes indicar el salto en el seq.
+# Para que i tome valores entre 1 y 10 con un salto de 2 hacemos:
+# for i in $(seq 1 2 10); do echo $i; done
+
 for i in $(seq 1 10); do
 	echo "La variable vale ${i}"
 done
